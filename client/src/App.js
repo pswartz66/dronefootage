7d41fabcab8722d5bba408b58c2c818cc90666ed
phil <pswartz.sju@gmail.com>
2020-10-25 13:49:23 -0400

    added main page and some styling, no routes setup yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import dfLogo from './assets/images/df_logo.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img alt="blank" src={dfLogo} 
+            style={{ 
+              height: '260px', 
+              width: '462px', 
+              borderRadius: '10px',
+              border: '3px solid #3D7AAC',
+              boxShadow: '0px 0px 80px 0px black'
+              }} />
+        <div className="App-signup-login">
+          <a className="signup" href="/signup">
+            Sign Up
         </a>
+        
+          <a className="login" href="/login">
+            Login
+        </a>
+        </div>
+
       </header>
     </div>
   );
