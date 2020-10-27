99d4a118e9b4195e4920c131adf9101a8bf26d5f
phil <pswartz.sju@gmail.com>
2020-10-26 23:00:38 -0400

    added styling to signup and ability to submit or log the inputs

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,44 +2,74 @@ import React, { useState } from 'react';
 import './Signup.css';
 
 const Signup = () => {
+    // initial variables
     let [firstName, setFirstName] = useState('');
     let [lastName, setLastName] = useState('');
     let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
 
+    const handleSubmit = () => {
+        // push data to an object -> to send through api
+        const obj = {
+            first_name: firstName,
+            last_name: lastName,
+            user_email: email,
+            user_password: password
+        }
+        console.log(obj)
+        
+        // reset input fields
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+    } 
 
     return (
         <div className="signup-container">
             <form className="signup-form">
                 <h1 className="signup-header">Signup</h1>
                 <div className="signup-divider" />
-                
+
                 <div className="signup-inputs">
+
                     <label for="fname">First name</label>
                     <input
                         type="text"
                         name="firstName"
-                        onChange={setFirstName}
+                        onChange={e => setFirstName(e.target.value)}
+                        value={firstName}
                         placeholder="First name..."
                     />
-                    <label for="lname">Last name</label>
 
+                    <label for="lname">Last name</label>
                     <input
                         type="text"
                         name="lastName"
-                        onChange={setLastName}
+                        onChange={e => setLastName(e.target.value)}
+                        value={lastName}
                         placeholder="Last name..."
                     />
-                    <label for="email">Email</label>
 
+                    <label for="email">Email</label>
                     <input
                         type="text"
                         name="email"
-                        onChange={setEmail}
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
                         placeholder="Email..."
                     />
 
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                        placeholder="Password..."
+                    />
                     <br />
-                    <button className="signup-btn" type="button">Submit</button>
+                    <button onClick={handleSubmit} className="signup-btn" type="button">Submit</button>
                 </div>
             </form>
         </div>
