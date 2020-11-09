a91b1c77f95db7413b750518602b9f63e70f57fb
phil <pswartz.sju@gmail.com>
2020-11-08 21:17:55 -0500

    added searchbar to navbar no functionality yet

--- client/src/components/PublicHeader/index.js
+++ client/src/components/PublicHeader/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './PublicHeader.css';
 import MdMenu from 'react-ionicons/lib/MdMenu';
+import appLogo from '../../assets/images/dfLogo.png';
+import MdSearch from 'react-ionicons/lib/MdSearch';
 
 const PublicHeader = (props) => {
 
@@ -9,13 +11,25 @@ const PublicHeader = (props) => {
     return (
         <nav className="navbar navbar-default navbar-fixed-top">
 
+            <img className="dfLogo" src={appLogo} alt="logo" />
+
+            <form>
+                <MdSearch fontSize="16px" color="#282c34" />
+
+                <input
+                    type="search"
+                    name="search"
+                    placeholder="Search"
+                    className="searchField"
+                />
+                
+            </form>
             <MdMenu fontSize="28px" color="white" />
             <div className="">{props.userName}</div>
 
         </nav>
 
     )
-
 }
 
 export default PublicHeader;
