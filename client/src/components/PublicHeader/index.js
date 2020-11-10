3a2b59ef0c6fcefe0bfcd72c90c822707c343c7f
phil <pswartz.sju@gmail.com>
2020-11-09 22:33:04 -0500

    added css to the public page

--- client/src/components/PublicHeader/index.js
+++ client/src/components/PublicHeader/index.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PublicHeader.css';
-import MdMenu from 'react-ionicons/lib/MdMenu';
 import appLogo from '../../assets/images/dfLogo.png';
 import MdSearch from 'react-ionicons/lib/MdSearch';
+import MdPerson from 'react-ionicons/lib/MdPerson';
 
 const PublicHeader = (props) => {
+
+    let [showMenu, setShowMenu] = useState(false);
+
+    const openMenu = () => setShowMenu(!showMenu);
+
+    // useEffect(() => {
+
+    // }, [showMenu]);
 
     console.log(props);
 
     return (
         <nav className="navbar navbar-default navbar-fixed-top">
 
-            <img className="dfLogo" src={appLogo} alt="logo" />
+            <div className="nav-left">
+                <img className="dfLogo" src={appLogo} alt="logo" />
 
-            <form>
-                <MdSearch fontSize="16px" color="#282c34" />
+                <form>
+                    <MdSearch fontSize="16px" color="#282c34" />
 
-                <input
-                    type="search"
-                    name="search"
-                    placeholder="Search"
-                    className="searchField"
-                />
-                
-            </form>
-            <MdMenu fontSize="28px" color="white" />
-            <div className="">{props.userName}</div>
+                    <input
+                        type="search"
+                        name="search"
+                        placeholder="Search"
+                        className="searchField"
+                    />
+
+                </form>
+            </div>
+
+            <div className="nav-right">
+                <MdPerson onClick={() => openMenu()} className="user-icon" color="#282C34" />
+                <div className="user-name">{props.userName}</div>
+
+                {(showMenu) ?
+                    <div className="drop-down-visible">
+                        <button className="view-profile-btn" type="submit">View Profile</button>
+                        <div className="dropdown-divider"></div>
+
+                        <div className="dropdown-header">Account</div>
+                        {/* <div> */}
+                            <div className="dropdown-sub">Settings</div>
+                            <div className="dropdown-sub">Help</div>
+
+                            <div className="dropdown-divider-t"></div>
+                            
+
+                            <div className="dropdown-header">Sign Out</div>
+                        {/* </div> */}
+
+                    </div>
+                    :
+                    null
+                }
+
+            </div>
 
         </nav>
 
