e64461ba238db04ba0903eb4c9cc75db2d723689
phil <pswartz.sju@gmail.com>
2020-11-16 17:40:21 -0500

    changed to navlink need to try isActiveFunc on it

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Settings.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import AccountInfo from '../components/AccountInfo';
 import Privacy from '../components/Privacy';
 import Help from '../components/Help';
@@ -15,15 +15,40 @@ const Settings = (props) => {
             </div>
             <div className="settings-flex-container">
                 <div className="settings-left-pane">
-                    <Link className="settings-left-pane-link" to="/settings">Account Information</Link>
-                    <Link className="settings-left-pane-link" to="/settings">Privacy</Link>
-                    <Link className="settings-left-pane-link" to="/settings">Help</Link>
-                    <Link className="settings-left-pane-link" to="/settings">Delete Account</Link>
+                    <NavLink 
+                        onClick={console.log('Account')}
+                        className="settings-left-pane-account" 
+                        to="/settings"
+                    >
+                        Account Information
+                    </NavLink>
+                    <NavLink 
+                        onClick={console.log('Privacy')}
+                        // onClick={console.log('privacy was clicked')} 
+                        className="settings-left-pane-privacy" 
+                        to="/settings"
+                    >
+                        Privacy
+                    </NavLink>
+                    <NavLink 
+                        onClick={console.log('Help')}
+                        className="settings-left-pane-help" 
+                        to="/settings"
+                    >
+                        Help
+                    </NavLink>
+                    <NavLink 
+                        onClick={console.log('Delete')}
+                        className="settings-left-pane-delete" 
+                        to="/settings"
+                    >
+                        Delete Account
+                    </NavLink>
                 </div>
 
                 <div className="settings-right-pane">
                     <Switch>
-                        <Route exact path="/settings" component={AccountInfo} />
+                        <Route path="/settings" render={() => <AccountInfo />} />
                         <Route path="/settings" component={Privacy} />
                         <Route path="/settings" component={Help} />
                         <Route path="/settings" component={DeleteAccount} />
